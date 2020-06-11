ace65bd4ee7ebc76b2798208a46740d94abea9a2
17jmalek98 <17jmalek98@gmail.com>
2020-06-11 15:53:48 -0700

    implemented user resolver

--- resolvers.js
+++ resolvers.js
@@ -3,24 +3,9 @@ import User from './schemas/user.schema'
 
 export const resolvers = {
   Query: {
-    getUser(_, {id}){
-      User.findById(id, function(err, user){
-        if(err){
-          console.log("Error");
-          return null;
-        }
-        else{
-          console.log(user);
-          return {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-            experience: user.experience,
-            medals: [],
-            total_donated: user.total_donated
-          };
-        }
-      });
+    getUser: async (_, {id}) => {
+      const user = await User.findById(id);
+      return user.toObject();
     }
   }
 
