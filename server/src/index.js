93d657647c898b070ef7ee5abcce2c67f75d1011
Abdul <muiz.rpi@gmail.com>
2020-06-18 09:43:47 -0400

    Twitter tests

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,20 @@ import {resolvers} from '../resolvers'
 import dotenv from 'dotenv'
 import { dailyNonprofitSelection } from '../cron_jobs/NonprofitSelection.cron'
 
+import Twitter from 'twitter'
+import TwitterLite from 'twitter-lite'
+
 dotenv.config()
 
 const startServer = async () => {
 
   const app = express ()
+  const loggingMiddleware = (req, res, next) => {
+    console.log(`IP: `, req.ip)
+    next()
+  }
+  app.use(loggingMiddleware)
+
   const server = new ApolloServer({
     typeDefs,
     resolvers
@@ -27,6 +36,37 @@ const startServer = async () => {
 
   // Start the cron jobs
   dailyNonprofitSelection.start()
+
+  // test Twitter
+  // let twitterClient = new Twitter({
+  //   consumer_key: "ElvTnb0OJ3J9DSF9cCI3HZXTl",
+  //   consumer_secret: "807do5gaUWt4q5WPZH4pvPOyGczFtyzRoEktlSbiJ0lFqSXcNM",
+  //   access_token_key: "1156293643847778304-ynj8MT2RMFWGKHaGNov3GmYJMEtPWI",
+  //   access_token_secret: "wQdphX3LTHkqfCaIEbC9HgKYq3lBD19mPBtUqjosjHiaK",
+  // })
+  // console.log(twitterClient)
+  // twitterClient.post('statuses/update', {status: "Sample Tweet through Twitter API!"}, (error, tweets, response) => {
+  //   if (error) console.log(error)
+  //   else {
+  //     console.log(tweets)
+  //     console.log(response)
+  //   }
+  // })
+
+  // test Twitter Lite
+  // console.log(`Configuring Twitter Lite...`)
+  // const twitterLiteClient = new TwitterLite({
+  //   consumer_key: 'ElvTnb0OJ3J9DSF9cCI3HZXTl',
+  //   consumer_secret: '807do5gaUWt4q5WPZH4pvPOyGczFtyzRoEktlSbiJ0lFqSXcNM'
+  // })
+
+  // twitterLiteClient.getRequestToken("https://donatio-site.herokuapp.com/twitter")
+  // .then(res => {
+  //   console.log(res)
+  // })
+  // .catch(err => {
+  //   console.log(err)
+  // })
 
 }
 
