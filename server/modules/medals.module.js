4e514189ad5260199a8299264dd18e744983b744
Muiz Yusuff <muiz.rpi@gmail.com>
2020-06-23 06:55:56 -0400

    Added 4 donation milestone medals

--- server/modules/medals.module.js
+++ server/modules/medals.module.js
@@ -1,100 +1,110 @@
 // Medals Module -> Determine which medals a user earns, given their past
 // information and their updated information
 
-import axios from 'axios'
-import MedalAPI from '../API/medals.api'
-import { STRING_REFS } from '../string.refs'
+import axios from "axios";
+import MedalAPI from "../API/medals.api";
+import { STRING_REFS } from "../string.refs";
 
-let medals_
+let medals_;
 const setupMedals = async () => {
-    return MedalAPI.getMedals()
-    .then(response => {
-        medals_ = response
-        if(medals_){
-            console.log(`Medal setup complete!`)
-            console.log(medals_)
-        }
-        else{
-            throw new Error(response.status)
-        }
+  return MedalAPI.getMedals()
+    .then((response) => {
+      medals_ = response;
+      if (medals_) {
+        console.log(`Medal setup complete!`);
+        console.log(medals_);
+      } else {
+        throw new Error(response.status);
+      }
     })
-    .catch(err => {
-        console.log(`Error in setupMedals ()`)
-        console.log(err)
-    })
-}
+    .catch((err) => {
+      console.log(`Error in setupMedals ()`);
+      console.log(err);
+    });
+};
 
-setupMedals()
+setupMedals();
 
-// this block is causing issues, we need to properly wrap an await incase 
-// that data is undefined.  Leaving it commented for now.  
+// this block is causing issues, we need to properly wrap an await incase
+// that data is undefined.  Leaving it commented for now.
 //setupMedals ()
 //.catch(err => {
 //    console.log(`[module:medals] Problem setting up Medals module.`)
 //})
 //.then(response => {
-    //response comes back clean
-    //console.log(`Medals Module Response`)
+//response comes back clean
+//console.log(`Medals Module Response`)
 //    medals_ = response.data.data.medals
-    //console.log(medals_)
+//console.log(medals_)
 //})
 
 //=========================================================================
 
 const rewardMedal = (process_func) => {
-    let reward_ = null
-    medals_.forEach(medal => {
-
-        if (medal.process_func == process_func) {
-            reward_ = { ...medal._doc }
-        }
-    })
-    return reward_
-}
+  let reward_ = null;
+  medals_.forEach((medal) => {
+    if (medal.process_func == process_func) {
+      reward_ = { ...medal._doc };
+    }
+  });
+  return reward_;
+};
 
 const hasMedal = (medals_arr, medal) => {
-    console.log(`In hasMedal...`)
+  console.log(`In hasMedal...`);
 
-    for (let medal_index in medals_arr) {
-        let user_medal = medals_arr[medal_index]
-        if (user_medal._id.equals(medal._id)) {
-            return true
-        }
+  for (let medal_index in medals_arr) {
+    let user_medal = medals_arr[medal_index];
+    if (user_medal._id.equals(medal._id)) {
+      return true;
     }
-    return false
-}
+  }
+  return false;
+};
 
 const processMedals = (user_data, normalized_donation_amount) => {
-    
-    let medals_earned = []
-    console.log(`Processing Medals`)
-    console.log(user_data)
-    medals_.forEach(medal => {
+  let medals_earned = [];
+  console.log(`Processing Medals`);
+  console.log(user_data);
+  medals_.forEach((medal) => {
+    let process_func = medal.process_func;
+    console.log(`Checking medal: ${medal.name} => ${process_func}`);
+    if (!hasMedal(user_data.medals, medal)) {
+      let medal_response = _process[process_func](
+        user_data,
+        normalized_donation_amount
+      );
 
-        let process_func = medal.process_func
-        console.log(`Checking medal: ${medal.name} => ${process_func}`)
-        if (!hasMedal(user_data.medals, medal)) {
-            let medal_response = _process[process_func](user_data, normalized_donation_amount)
+      if (medal_response) medals_earned.push(medal_response);
+    }
+  });
 
-        if (medal_response) medals_earned.push(medal_response)
-        }
-    })
-
-    console.log(`Medals Earned`)
-    console.log(medals_earned)
-    return medals_earned
-}
+  console.log(`Medals Earned`);
+  console.log(medals_earned);
+  return medals_earned;
+};
 
 const _process = {
-    _first_donation: (user_data, normalized_donation_amount) => {
-        // Determine whether the user earns their first donation
-        // medal.
-        console.log(`Processing first donation!`)
+  _first_donation: (user_data, normalized_donation_amount) => {
+    // Determine whether the user earns their first donation
+    // medal.
+    console.log(`Processing first donation!`);
 
-        console.log(`First donation ? ${user_data.total_donated == 0}`)
-        if (user_data.total_donated == 0) return rewardMedal('_first_donation')
+    console.log(`First donation ? ${user_data.total_donated == 0}`);
+    if (user_data.total_donated == 0) return rewardMedal("_first_donation");
+  },
+  _milestone_one: (user_data, normalized_donation_amount) => {
+    if (user_data.total_donated >= 5) return rewardMedal("_milestone_one");
+  },
+  _milestone_two: (user_data, normalized_donation_amount) => {
+    if (user_data.total_donated >= 10) return rewardMedal("_milestone_one");
+  },
+  _milestone_three: (user_data, normalized_donation_amount) => {
+    if (user_data.total_donated >= 20) return rewardMedal("_milestone_one");
+  },
+  _milestone_four: (user_data, normalized_donation_amount) => {
+    if (user_data.total_donated >= 40) return rewardMedal("_milestone_one");
+  },
+};
 
-    }
-}
-
-export { processMedals }
+export { processMedals };
--- server/schemas/medal.schema.js
+++ server/schemas/medal.schema.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 var Schema = mongoose.Schema;
 
 var MedalSchema = new Schema({
-    name: String,
-    description: String,
-    img_url: String,
-    process_func: String
-})
+  name: String,
+  description: String,
+  alt_description: String,
+  asset_key: String,
+  process_func: String,
+});
 
-const Medal = mongoose.model('Medal', MedalSchema);
+const Medal = mongoose.model("Medal", MedalSchema);
 
 module.exports = Medal;
