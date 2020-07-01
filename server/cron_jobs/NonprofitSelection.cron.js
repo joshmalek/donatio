a799e7815dcb5e7388f09c498f31bcdc31a78699
17jmalek98 <17jmalek98@gmail.com>
2020-07-01 14:40:49 -0700

    finalize structure

--- server/cron_jobs/NonprofitSelection.cron.js
+++ server/cron_jobs/NonprofitSelection.cron.js
@@ -1,8 +1,8 @@
 import cron from 'cron'
 import axios from 'axios'
 
-//run everyday at midnight
-const dailyNonprofitSelection = new cron.CronJob('0 0 * * *', () => {
+//run everyday at 4:59 pm
+const dailyNonprofitSelection = new cron.CronJob('59 16 * * *', () => {
   console.log(`\nDailyNonprofitSelection Cron Job Running`)
   let start_time = new Date ()
   console.log(`${start_time.getMonth() + 1}/${start_time.getDay()}/${start_time.getFullYear()}`)
@@ -27,6 +27,7 @@ const dailyNonprofitSelection = new cron.CronJob('0 0 * * *', () => {
 const dailyDonationTweet = new cron.CronJob('0 17 * * *', () => {
   console.log("Tweeting our daily donation totals\n");
 
+  //change this to retrieve npo of day, new npo, and
   axios.post('http://localhost:4000/graphql',{
     query: '{nonprofits{name,priority,id}}'
   })
@@ -34,8 +35,8 @@ const dailyDonationTweet = new cron.CronJob('0 17 * * *', () => {
     let nonprofits = res.data.data.nonprofits
     //retrieve nonprofit of the day
     //tweet structure
-    //  Today DonatIO users donated ${total} to {NPOofDay}!  Thank you to everyone who donated.
-    //  Our new nonprofit will be {new_npoOfDay}.  Let's help them out! 
+    console.log("Today DonatIO users donated ${total} to {NPOofDay}!  Thank you to everyone who donated.")
+    console.log("Our new nonprofit will be {new_npoOfDay}.  Let's help them out!")
   })
   .catch(err => {
     console.log('Error fetching nonprofits...')
