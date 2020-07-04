3896c56120ba92955d296a185b3a2fe288104277
17jmalek98 <17jmalek98@gmail.com>
2020-07-03 17:21:16 -0700

    transferred to api scheme

--- server/cron_jobs/NonprofitSelection.cron.js
+++ server/cron_jobs/NonprofitSelection.cron.js
@@ -7,7 +7,7 @@ const dailyNonprofitSelection = new cron.CronJob("59 16 * * *", () => {
   let start_time = new Date();
   console.log(
     `${
-      start_time.getMonth() + 1
+    start_time.getMonth() + 1
     }/${start_time.getDay()}/${start_time.getFullYear()}`
   );
   var url = "http://localhost:4000/graphql";
@@ -66,76 +66,58 @@ const dailyDonationTweet = new cron.CronJob("0 17 * * *", async () => {
   let receipts = await getReceipt();
   let npo = await getNPO();
 
-  //
+  var today = new Date();
+  var yesterday = new Date(today);
 
-  axios
-    .get("http://localhost:4000/graphql?query={receipts{amount,date_time}}")
-    .then((res) => {
-      let receipts = res.data.data.receipts;
-      var today = new Date();
-      var yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
 
-      yesterday.setDate(yesterday.getDate() - 1);
+  //Date.parse converts to int format
+  today.toDateString();
+  today = Date.parse(today);
+  yesterday.toDateString();
+  yesterday = Date.parse(yesterday);
+  var total_donations_today = 0;
+  for (var i = 0; i < receipts.length; i++) {
+    var parsed_date = Date.parse(receipts[i].date_time);
+    //for each receipt, see if it falls between today and yesterday at the time the cron job is run (5pm)
+    if (parsed_date > yesterday && parsed_date < today) {
+      console.log(receipts[i].date_time + " " + receipts[i].amount);
+      total_donations_today += receipts[i].amount;
+    } else {
+      console.log(
+        "NOT IN TIMESPAN " +
+        receipts[i].date_time +
+        " " +
+        receipts[i].amount
+      );
+    }
+  }
+  console.log(
+    "total donated to " + npo + " = " + total_donations_today
+  );
 
-      //Date.parse converts to int format
-      today.toDateString();
-      today = Date.parse(today);
-      yesterday.toDateString();
-      yesterday = Date.parse(yesterday);
-      var total_donations_today = 0;
-      for (var i = 0; i < receipts.length; i++) {
-        var parsed_date = Date.parse(receipts[i].date_time);
-        //for each receipt, see if it falls between today and yesterday at the time the cron job is run (5pm)
-        if (parsed_date > yesterday && parsed_date < today) {
-          console.log(receipts[i].date_time + " " + receipts[i].amount);
-          total_donations_today += receipts[i].amount;
-        } else {
-          console.log(
-            "NOT IN TIMESPAN " +
-              receipts[i].date_time +
-              " " +
-              receipts[i].amount
-          );
-        }
+  //tweet structure
+  client.post(
+    "statuses/update",
+    {
+      status:
+        "Today DonatIO users donated $" +
+        total_donations_today +
+        " to " +
+        npo +
+        "!  Thank you to everyone who donated. Our new nonprofit will be " +
+        npo +
+        ".  Let's help them out!",
+    },
+    function (error, tweet, response) {
+      if (!error) {
+        console.log("Tweet successfully posted\n");
+      } else {
+        console.log(error);
       }
-      //retrieve nonprofit of the day
-      axios
-        .get("http://localhost:4000/graphql?query={NPOofDay{name}}")
-        .then((res) => {
-          var npo = res.data.data.NPOofDay.name;
-          console.log(
-            "total donated to " + npo + " = " + total_donations_today
-          );
-
-          //tweet structure
-          client.post(
-            "statuses/update",
-            {
-              status:
-                "Today DonatIO users donated $" +
-                total_donations_today +
-                " to " +
-                npo +
-                "!  Thank you to everyone who donated. Our new nonprofit will be " +
-                npo +
-                ".  Let's help them out!",
-            },
-            function (error, tweet, response) {
-              if (!error) {
-                console.log("Tweet successfully posted\n");
-              } else {
-                console.log(error);
-              }
-            }
-          );
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((err) => {
-      console.log("Error fetching receipts...");
-      console.log(err);
-    });
+    }
+  );
 });
+
+
 export { dailyNonprofitSelection, dailyDonationTweet };
